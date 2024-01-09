45669a4a3e8c21b481d5bdc0c08d115e00fc0103
aply333 <andrei@aply.tech>
2024-01-08 18:46:16 -0800

    VERCEL DEPLOY CONFIGS

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,15 @@
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => (
+    {
     "vercel-deploy": {
       enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_F39xTNaZERbgogM7YOwMAYYnCFsY/dirmWAIcDd",
+        apiToken: process.env.VERCEL_TOKEN,
+        appFilter: "aply",
+        // teamFilter: "your-team-id-on-vercel",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+       
+      },
     },
   });
