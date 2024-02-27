90974dfe90dd9ec5ab461108469d5d6c9407dbc3
aply333 <andrei@aply.tech>
2024-02-27 08:03:02 -0800

    content updates

---
+++ src/api/page-gallery/routes/page-gallery.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-gallery router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::page-gallery.page-gallery');
---
+++ src/api/page-gallery/services/page-gallery.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-gallery service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::page-gallery.page-gallery');
